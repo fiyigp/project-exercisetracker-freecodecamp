9c1f260a636a53610cff3b101d73f560579cfa76
Alfredo Garcia <alfredo.garcia@disoftec.mx>
2024-06-17 17:02:21 -0600

    added enpoints with local vars

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const bodyParser = require('body-parser');
+const users = {};
+const exercises = {};
+
 require('dotenv').config()
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(cors())
 app.use(express.static('public'))
@@ -9,9 +15,39 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+app.post('/api/users', (req, res) => {
+  const user = req.body.username;
+  const id = Object.entries(users).length + 1;
+  users[id] = {username: user, _id: id};
+  res.json({username: user, _id: id});
+});
 
+app.get('/api/users', (req, res) => {
+  res.json(Object.values(users));
+});
 
+app.post('/api/users/:_id/exercises', (req, res) => {
+  const user = users[req.params._id];
+  const date = (req.params.date) ? new Date(req.params.date) : new Date();
+  const exercise = {description: req.body.description, duration: req.body.duration, date: date.toDateString()}
+  
+  if (exercises[user._id] === undefined) {
+    exercises[user._id] = []; 
+  }
 
+  exercises[user._id].push(exercise);
+  exercise.username = user.username;
+  exercise._id = user._id;
+
+  res.json();
+});
+
+app.get('/api/users/:_id/logs', (req, res) => {
+  const user = users[req.params._id];
+  const userExercises = exercises[user._id];
+  
+  res.json({username: user.username, _id: user._id, count: userExercises.length, log: userExercises});
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
